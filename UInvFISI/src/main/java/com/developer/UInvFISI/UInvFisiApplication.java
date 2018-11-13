58ddd159f63970c9d551a7c0b0171be1ba9ea9e9
Alexis Manuel Gutierrez Fuentes <=>
2018-11-13 14:30:07 -0500

    Subida de la Estructura del Proyecto

---
+++ UInvFISI/src/main/java/com/developer/UInvFISI/UInvFisiApplication.java
@@ -0,0 +1,12 @@
+package com.developer.UInvFISI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UInvFisiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UInvFisiApplication.class, args);
+	}
+}
---
+++ UInvFISI/src/test/java/com/developer/UInvFISI/UInvFisiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.developer.UInvFISI;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class UInvFisiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
