9e3c4d440e8cff9216ced5e6f477077825580afa
Franco Perez Samillan <francotac19@hotmail.com>
2018-11-19 19:59:49 -0500

    configuracion aplication properties

---
+++ UInvFISI/src/main/java/com/developer/UInvFISI/dao/DocumentoDao.java
@@ -0,0 +1,8 @@
+package com.developer.UInvFISI.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.developer.UInvFISI.entity.Documento;
+
+public interface DocumentoDao extends JpaRepository<Documento, Integer>{
+}
