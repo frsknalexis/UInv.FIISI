35d522834d5bf8f2f27122fe02c9dc9f8087d999
dubal17 <francotac19@hotmail.com>
2019-02-07 04:01:06 -0500

    Scrip actualizado

--- ScripFranco.sql
+++ ScripFranco.sql
@@ -42,4 +42,20 @@ CREATE TABLE tbl_autor
       REFERENCES tbl_documentos (documento_id) MATCH SIMPLE
       ON UPDATE NO ACTION ON DELETE NO ACTION
 );
-alter sequence tbl_autor_autor_id_seq rename to autor_seq
+
+
+CREATE TABLE tbl_informes_trabajo
+(
+  informe_trabajo_id serial not null,
+  trabajo_id integer,
+  asunto character varying(100),
+  nombre_fichero character varying(75),
+  tamanio_fichero character varying(10),
+  formato_fichero character varying(75),
+  fecha_registro timestamp without time zone,
+  fecha_modificacion timestamp without time zone,
+  habilitado boolean DEFAULT true,
+  CONSTRAINT pk_informes_trabajo PRIMARY KEY (informe_trabajo_id),
+  CONSTRAINT fk_trabajoinf FOREIGN KEY (trabajo_id) references tbl_trabajo (trabajo_id)
+)
+alter sequence tbl_informes_trabajo_informe_trabajo_id_seq rename to informe_trabajo_seq
