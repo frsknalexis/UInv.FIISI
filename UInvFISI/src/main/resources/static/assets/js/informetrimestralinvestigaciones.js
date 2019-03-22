80f27828803609fbde8b2e2c189f436aba58fdf7
Alexis Manuel <frsknalexis.1997@gmail.com>
2019-03-22 11:50:44 -0500

    Servicio RestFull para el modulo de informe trimestral y subida de archivos OK

--- UInvFISI/src/main/resources/static/assets/js/informetrimestralinvestigaciones.js
+++ UInvFISI/src/main/resources/static/assets/js/informetrimestralinvestigaciones.js
@@ -14,6 +14,8 @@ $(document).on('ready', function() {
 		
 		var contenidoInvestigacionInformeInvestigadores = document.querySelector('#contenidoInvestigacionInformeInvestigadores');
 		
+		var contenidoInvestigacionInformeInformesTrimestrales = document.querySelector('#contenidoInvestigacionInformeInformesTrimestrales');
+		
 		contenidoInvestigacion.innerHTML = '';
 		
 		$.ajax({
@@ -27,7 +29,7 @@ $(document).on('ready', function() {
 				$('#titleDetalleInvestigacionInformeModal').html('PROYECTO DE INVESTIGACION ' + response.anio);
 				$('#detalleNombreInvestigacionInforme').html(response.nombreInvestigacion);
 				$('#alertDetalleInvestigacionInformeInvestigadores').html('Aun no hay Investigadores asignados al Proyecto de Investigacion: <strong>' + response.nombreInvestigacion + '</strong>');
-				$('#alertDetalleInvestigacionInformesTrimestrales').html('Aun no hay Informes Trimestrales adjuntados al Proyecto de Investigacion: <strong>' + response.nombreInvestigacion + '</strong>');
+				$('#alertDetalleInvestigacionInformeInformesTrimestrales').html('Aun no hay Informes Trimestrales adjuntados al Proyecto de Investigacion: <strong>' + response.nombreInvestigacion + '</strong>');
 				contenidoInvestigacion.innerHTML += '<tr><td>' + response.lineaInvestigacion.programa.nombrePrograma + '</td><td>' + response.lineaInvestigacion.nombreLineaInvestigacion + '</td></tr>';
 			}
 		});
@@ -48,7 +50,7 @@ $(document).on('ready', function() {
 					for(var i = 0; i < response.length; i++) {
 						console.log(response[i]);
 						contenidoInvestigacionInformeInvestigadores.innerHTML += '<tr>';
-						contenidoInvestigacionInformeInvestigadores.innerHTML += '<td>'+ response[i].investigador +'</td><td>'+ response[i].facultad.abreviaturaFacultad +'</td><td>'+ response[i].condicion.nombreCondicion +'</td>';
+						contenidoInvestigacionInformeInvestigadores.innerHTML += '<td>'+ (i+1) +'</td><td>'+ response[i].investigador +'</td><td>'+ response[i].facultad.abreviaturaFacultad +'</td><td>'+ response[i].condicion.nombreCondicion +'</td>';
 						contenidoInvestigacionInformeInvestigadores.innerHTML += '</tr>';
 					}
 				}
@@ -68,8 +70,35 @@ $(document).on('ready', function() {
 			dataType: 'json',
 			success: function(response) {
 				
-				console.log(response);
+				if(response != null) {
+					
+					$('#tablaInvestigacionInformeInformesTrimestrales').show();
+					$('#alertDetalleInvestigacionInformeInformesTrimestrales').hide();
+					
+					contenidoInvestigacionInformeInformesTrimestrales.innerHTML = '';
+					for(var i = 0; i < response.length; i++) {
+						console.log(response[i]);
+						contenidoInvestigacionInformeInformesTrimestrales.innerHTML += '<tr>';
+						contenidoInvestigacionInformeInformesTrimestrales.innerHTML += '<td>'+ (i+1) +'</td><td>'+ response[i].descripcion +'</td><td>'+ response[i].trimestre +'</td><td>'+ response[i].nombreFichero +'</td><td><div class="btn-group" role="group"><a href="/api/informetrimestral/view/' + response[i].nombreFichero +  '" target="_blank" class="btn btn-outline-info btn-xs"><i class="fa fa-eye" title="Visualizar PDF"></i></a><button id="downloadFileInformeTrimestral" nombreFicheroInformeTrimestral="' + response[i].nombreFichero +'" class="btn btn-outline-primary btn-xs"><i class="fa fa-download" title="Descargar"></i></button></div></td>';
+						contenidoInvestigacionInformeInformesTrimestrales.innerHTML += '</tr>';
+					}
+				}
+				else {
+					
+					$('#tablaInvestigacionInformeInformesTrimestrales').hide();
+					$('#alertDetalleInvestigacionInformeInformesTrimestrales').show();
+					console.log('No hay datos');
+				}
+				
 			}
 		});
+	});
+	
+	$('#tablaInvestigacionInformeInformesTrimestrales tbody').on('click', 'button#downloadFileInformeTrimestral', function() {
+		
+		var nombreFicheroInformeTrimestral = $(this).attr('nombreficheroinformetrimestral');
+		console.log('nombreFicheroInformeTrimestral: ' + nombreFicheroInformeTrimestral);
+		
+		$(location).attr('href', '/api/informetrimestral/download/' + nombreFicheroInformeTrimestral);
 	});
 });
